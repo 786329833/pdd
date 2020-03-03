96816a7018487ef7e5877b4bed54af5d4d0fb98e
LengPengfei <786329833@qq.com>
2020-03-03 08:37:11 +0800

    haha

--- src/app/home/components/home-detail/home-detail.component.ts
+++ src/app/home/components/home-detail/home-detail.component.ts
@@ -15,7 +15,7 @@ export class HomeDetailComponent implements OnInit {
   username = '';
   selectedTablink;
   imageSliders: ImageSlide[] = [];
-  dd = 1;
+  tt = 1;
 
   ngOnInit() {
     this.service.getImageSlider().subscribe(sliders => {
